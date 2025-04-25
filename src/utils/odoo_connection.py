fb9969e9d9a297424a8193445e50cfd2c9a9ed2c
Miguel Oscar <miguel.almarales@inscomexico.com>
2025-04-25 16:02:30 -0600

    Se añaden archivos para análisis de textos

--- src/utils/odoo_connection.py
+++ src/utils/odoo_connection.py
@@ -12,4 +12,32 @@ def connect_to_odoo_api(path, parameters):
     """
     # Function code goes here
 
-    
+    url = 'http://example.com:8069/'
+    db = 'sample_db'
+    username = 'sample_user'
+    password = 'sample_password'
+
+    session_url = f'{url}/web/session/authenticate'
+    data = {
+        'jsonrpc': '2.0',
+        'method': 'call',
+        'params': {
+            'db': db,
+            'login': username,
+            'password': password,
+        }
+    }
+    session_response = requests.post(session_url, json=data)
+    session_data = session_response.json()
+
+    if session_data.get('result') and session_response.cookies.get('session_id'):
+        session_id = session_response.cookies['session_id']
+    else:
+        print(f'Error: Failed to authenticate - {session_data.get("error")}')
+        return None
+
+
+headers = {
+    'Content-Type': 'application/json',
+    'Cookie': f"session_id={session_id}",
+}
